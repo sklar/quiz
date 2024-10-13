2f889a5149ba63078cefbeffadb59d9ffa808b8a
David Sklář <david@sklar.xyz>
2024-10-13 19:10:05 +0200

    feat: svelte app

---
+++ packages/svelte/README.md
@@ -0,0 +1,7 @@
+# Svelte playground 🕹️
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/75d6fa26-1c5b-4c25-be27-572499bf3f87/deploy-status)](https://app.netlify.com/sites/quiz-svelte-4be8d4/deploys)
+
+A super simple Svelte playground to learn the basics of popular frameworks.
+
+<img src="../../resources/svelte.svg" alt="Svelte logo" width="80" height="80">
---
+++ packages/svelte/src/quiz/QuizProgress.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import type { Question } from './quiz.controller'
+
+export let questions: Question[]
+</script>
+
+<p class="result" aria-label="Correct answers of all answers">
+	{questions.filter((q) => q.isCorrect).length} of {questions.length}
+</p>
+
+<style>
+	.result {
+		display: grid;
+		gap: var(--size-8);
+		justify-items: center;
+	}
+</style>
